f5b27dd056a5e6b47b42926549f5e3c58c4c3f37
Chris Coudron <coudron@gmail.com>
2023-06-28 12:34:48 -0400

    adding sub heading to logo

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -23,7 +23,7 @@ export default function Nav() {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img className="h-8 w-auto" src="logos/logo.svg" alt="" />
+            <img className="h-8 w-auto" src="logos/logo-subheading.svg" alt="" />
           </a>
         </div>
         <div className="flex lg:hidden">
