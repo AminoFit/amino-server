9568bc45870d1ec8d62637d5d14766cc9bbe3980
Chris Coudron <coudron@gmail.com>
2023-06-29 18:50:33 -0400

    Cleanup login with code page

--- src/app/login-code/page.tsx
+++ src/app/login-code/page.tsx
@@ -80,89 +80,46 @@ export default function SignInCode() {
               }}
             >
               {(formik) => (
-                <form onSubmit={formik.handleSubmit}>
-                  <div
-                    className="bg-red-400 flex flex-col items-center 
-            justify-center min-h-screen py-2 shadow-lg"
-                  >
-                    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                      <input
-                        name="csrfToken"
-                        type="hidden"
-                        defaultValue={csrfData.csrfToken}
+                <form onSubmit={formik.handleSubmit} className="space-y-6">
+                  <input
+                    name="csrfToken"
+                    type="hidden"
+                    defaultValue={csrfData.csrfToken}
+                  />
+
+                  <div>
+                    <label
+                      htmlFor="code"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Enter SMS Code
+                    </label>
+                    <div className="mt-2">
+                      <Field
+                        name="code"
+                        aria-label="Enter your sms code"
+                        aria-required="true"
+                        type="text"
+                        placeholder="12345678"
+                        className="block w-full rounded-md border-0 px-4 py-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
-
-                      <div className="text-red-400 text-md text-center rounded p-2">
-                        {error}
-                      </div>
-                      <div className="mb-4">
-                        <label
-                          htmlFor="email"
-                          className="uppercase text-sm text-gray-600 font-bold"
-                        >
-                          SMS Code
-                          <Field
-                            name="code"
-                            aria-label="enter your sms code"
-                            aria-required="true"
-                            type="text"
-                            className="w-full bg-gray-300 text-gray-900 mt-2 p-3"
-                          />
-                        </label>
-
-                        <div className="text-red-600 text-sm">
-                          <ErrorMessage name="code" />
-                        </div>
-                      </div>
-                      <div className="flex items-center justify-center">
-                        <button
-                          type="submit"
-                          className="bg-green-400 text-gray-100 p-3 rounded-lg w-full"
-                        >
-                          {formik.isSubmitting ? "Please wait..." : "Sign In"}
-                        </button>
-                      </div>
                     </div>
+                    <div className="text-pink-600 text-sm mt-2">
+                      <ErrorMessage name="code" />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                    >
+                      {formik.isSubmitting ? "Please wait..." : "Submit Code"}
+                    </button>
                   </div>
                 </form>
               )}
             </Formik>
-
-            <form className="space-y-6" action="#" method="POST">
-              <div>
-                <label
-                  htmlFor="code"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Enter SMS Code
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="code"
-                    name="code"
-                    type="string"
-                    autoComplete="code"
-                    value={codeInput}
-                    onChange={(e) => setCodeInput(e.target.value)}
-                    required
-                    className="block w-full rounded-md border-0 px-4 py-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-                {error && (
-                  <div className="text-pink-600 text-sm mt-2">{error}</div>
-                )}
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  // onClick={onSubmit}
-                  className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                >
-                  Submit Code
-                </button>
-              </div>
-            </form>
 
             <p className="mt-10 text-center text-sm text-gray-500">
               Don't have a code yet?{" "}
