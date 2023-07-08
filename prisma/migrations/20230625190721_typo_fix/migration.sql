88609efc0c24ea2c860312c4867c269b80635b93
Chris Coudron <coudron@gmail.com>
2023-07-07 20:25:20 -1000

    Kill dev migrations

--- prisma/migrations/20230625190721_typo_fix/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `dateOfBrith` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "dateOfBrith",
-ADD COLUMN     "dateOfBirth" TIMESTAMP(3);
