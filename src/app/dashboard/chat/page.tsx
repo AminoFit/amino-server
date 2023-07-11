34716c1df92ff5b163ee18e941d06fa9763fbc3a
Seb <sebgrubb@gmail.com>
2023-07-11 11:24:45 -0400

    Ensure input box stays at bottom

--- src/app/dashboard/chat/page.tsx
+++ src/app/dashboard/chat/page.tsx
@@ -22,25 +22,30 @@ async function getMessages() {
 
 export default async function Chat() {
   const messages = await getMessages();
-  console.log("messages", messages);
+  // console.log("messages", messages);
 
   return (
-    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-      <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg leading-6 font-medium text-gray-900">
-          Chat with Amino
-        </h3>
+    <div className="flex flex-col h-full justify-between border divide-y divide-gray-200 rounded-lg bg-white shadow overflow-hidden" style={{height: 'calc(100vh - 2*2.5rem)'}}>
+      <div className="sticky top-0 bg-white z-10">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
+            Chat with Amino
+          </h3>
+        </div>
       </div>
-      <div className="px-4 py-5 sm:p-6 max-h-fit">
-        <div className="flex flex-col h-full overflow-y-auto">
+      <div className="overflow-y-auto">
+        <div className="flex flex-col">
           {messages.map((message) => (
             <ChatMessage message={message} key={message.id} />
           ))}
         </div>
       </div>
-      <div className="px-4 py-4 sm:px-6">
-        <ChatBox />
+      <div className="sticky bottom-0 bg-white z-10">
+        <div className="px-4 py-4 sm:px-6 bg-white">
+          <ChatBox />
+        </div>
       </div>
     </div>
-  );
+  );  
 }
+
--- src/components/chat/ChatBox.tsx
+++ src/components/chat/ChatBox.tsx
@@ -60,7 +60,7 @@ export default function ChatBox() {
               onChange={(e) => setMessageInput(e.target.value)}
               onKeyDown={handleKeyDown}
               ref={textareaRef}
-              style={{resize: 'none', overflow: 'hidden'}}
+              style={{resize: 'vertical', overflow: 'hidden', maxHeight: '30vh'}}
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Log that I ate a banana"
             />
