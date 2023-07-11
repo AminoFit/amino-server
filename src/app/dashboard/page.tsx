6540f6782d618e9d43b1c684460f4abe808afd49
Chris Coudron <coudron@gmail.com>
2023-07-11 12:37:34 -0400

    Fix scroll

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -37,7 +37,7 @@ export default async function FoodLog() {
         <div className="mt-8 flow-root">
           <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+              <div className="shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
                 <FoodTable foods={foods} />
               </div>
             </div>
