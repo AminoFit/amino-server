8b32a3595d97d2e896b5839e0e35e04e9f4f56ef
Chris Coudron <coudron@gmail.com>
2023-07-10 23:59:27 -0400

    Add logout in dashboard

--- src/app/dashboard/SideNav.tsx
+++ src/app/dashboard/SideNav.tsx
@@ -3,6 +3,7 @@
 import { Fragment, useState } from "react";
 import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
+  ArrowLeftOnRectangleIcon,
   Bars3Icon,
   BellIcon,
   CalendarIcon,
@@ -19,6 +20,7 @@ import {
   MagnifyingGlassIcon,
 } from "@heroicons/react/20/solid";
 import classNames from "classnames";
+import { signOut } from "next-auth/react";
 
 const navigation = [
   { name: "Food Log", href: "#", icon: HomeIcon, current: true },
@@ -243,16 +245,16 @@ export default function SideNav() {
                 </ul>
               </li>
               <li className="mt-auto">
-                <a
-                  href="#"
+                <button
+                  onClick={() => signOut({ callbackUrl: "/" })}
                   className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-indigo-200 hover:bg-indigo-700 hover:text-white"
                 >
-                  <Cog6ToothIcon
+                  <ArrowLeftOnRectangleIcon
                     className="h-6 w-6 shrink-0 text-indigo-200 group-hover:text-white"
                     aria-hidden="true"
                   />
-                  Settings
-                </a>
+                  Logout
+                </button>
               </li>
             </ul>
           </nav>
