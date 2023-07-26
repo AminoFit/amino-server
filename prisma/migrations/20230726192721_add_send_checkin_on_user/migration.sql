698a45b80c85a8ceae8065b743ed6c303546f2c4
Chris Coudron <coudron@gmail.com>
2023-07-26 15:50:13 -0400

    Adding cron event to send checkin text to user

---
+++ prisma/migrations/20230726192721_add_send_checkin_on_user/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "sendCheckins" BOOLEAN NOT NULL DEFAULT false;
--- src/app/api/cron/send-checkin-texts/route.ts
+++ src/app/api/cron/send-checkin-texts/route.ts
@@ -1,5 +1,40 @@
+import { prisma } from "@/database/prisma"
+import { SaveAndSendMessageToUser } from "@/twilio/SendMessageToUser"
+import { User } from "@prisma/client"
+import moment from "moment-timezone"
 import { NextResponse } from "next/server"
 
 export async function GET() {
-  return NextResponse.json({ ok: true })
+  // Query all users who have no message in the past N time?
+
+  const delaySinceLastMessage = 1000 * 60 // 60 seconds
+
+  const users = await prisma.user.findMany({
+    where: {
+      Message: {
+        none: {
+          createdAt: {
+            gt: new Date(Date.now() - delaySinceLastMessage)
+          }
+        }
+      },
+      sendCheckins: true
+    }
+  })
+
+  await Promise.all(users.map((user) => sendCheckinText(user)))
+
+  //
+  return NextResponse.json({ ok: true, users })
+}
+
+async function sendCheckinText(user: User) {
+  const userTime = moment().tz(user.tzIdentifier)
+
+  // Between 9am and 8pm local time for the user
+  if (userTime.hour() > 9 && userTime.hour() < 20) {
+    const message = `Hi there. Amino here. How are you doing? Any food you want to log?`
+
+    return SaveAndSendMessageToUser(user, message)
+  }
 }
