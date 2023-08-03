cebfa7dd19e1631e4a0248f602f243d23d2c6555
Chris Coudron <coudron@gmail.com>
2023-08-03 12:34:12 -0400

    Adding test endpoint for embeddings

---
+++ src/app/api/test-embedding/route.ts
@@ -0,0 +1,18 @@
+import { openai } from "@/utils/openaiFunctionSchemas"
+import { NextResponse } from "next/server"
+
+export async function GET() {
+  console.log("Testing getting an embedding from open AI")
+
+  const startTime = Date.now()
+  const embedding = await openai.createEmbedding({
+    model: "text-embedding-ada-002",
+    input: "Starbucks Iced Coffee with Classic Syrup",
+  })
+  const endTime = Date.now()
+  return NextResponse.json({
+    text: "get ok",
+    requestTime: endTime - startTime,
+    embedding: embedding.data
+  })
+}
