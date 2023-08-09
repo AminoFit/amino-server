bcd2d325ad31be84241512551058c75b1606f807
Chris Coudron <coudron@gmail.com>
2023-08-09 12:26:28 -0400

    Update the users name

--- src/app/dashboard/FoodLogHeader.tsx
+++ src/app/dashboard/FoodLogHeader.tsx
@@ -1,7 +1,11 @@
 import { LoggedFoodItem, FoodItem, User } from "@prisma/client"
 
 import FoodStats from "./FoodStats"
-import { ChevronRightIcon, FireIcon, TagIcon } from "@heroicons/react/24/outline"
+import {
+  ChevronRightIcon,
+  FireIcon,
+  TagIcon
+} from "@heroicons/react/24/outline"
 
 type LoggedFoodItemWithFoodItem = LoggedFoodItem & { FoodItem: FoodItem }
 
@@ -17,11 +21,13 @@ export function FoodLogHeader({
       <div className="">
         <div className="mt-5 flex mb-4">
           <div>
-            <div className="text-sm font-light text-amino-logo">
-              Welcome Back
-            </div>
+            {user.firstName && (
+              <div className="text-sm font-light text-amino-logo">
+                Welcome Back
+              </div>
+            )}
             <h2 className="mb-5 text-4xl font-bold leading-7 text-zinc-50 sm:truncate sm:text-3xl sm:tracking-tight">
-              Chris
+              {user.firstName || "Welcome Back"}
             </h2>
             <h2 className="mb-5 text-2xl font-bold leading-7 text-zinc-50">
               You're on track to meet your goals today!
@@ -31,11 +37,13 @@ export function FoodLogHeader({
 
         <div className="flex items-center py-3 px-4 bg-amino-logo text-zinc-900 rounded-xl">
           <div className="mr-3">
-          <FireIcon className="h-6 w-6" />
+            <FireIcon className="h-6 w-6" />
           </div>
           <div className="grow">
             <div className="text-xs uppercase">YOUR GOAL</div>
-            <div className="text-xl uppercase tracking-tighter">{user.fitnessGoal}</div>
+            <div className="text-xl uppercase tracking-tighter">
+              {user.fitnessGoal}
+            </div>
           </div>
           <div>
             <ChevronRightIcon className="h-6 w-6" />
