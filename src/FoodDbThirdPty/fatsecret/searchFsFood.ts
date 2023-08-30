a9db429bf21863c4381249059a1575a69032928a
Seb <sebgrubb@gmail.com>
2023-08-30 17:01:50 +0100

    get token in better way for debug

--- src/FoodDbThirdPty/fatsecret/searchFsFood.ts
+++ src/FoodDbThirdPty/fatsecret/searchFsFood.ts
@@ -16,12 +16,7 @@ export async function findFatSecretFoodInfo(
 
   console.log("Searching FatSecret for", searchParams.search_expression)
   let token;
-  try {
-    token = await getFsAccessToken();
-  } catch (error) {
-    //curl -u YOUR_CLIENT_ID:YOUR_CLIENT_SECRET -d "grant_type=client_credentials&scope=premier" -X POST https://oauth.fatsecret.com/connect/token -H "Content-Type: application/x-www-form-urlencoded"
-    token = `eyJhbGciOiJSUzI1NiIsImtpZCI6IjVGQUQ4RTE5MjMwOURFRUJCNzBCMzU5M0E2MDU3OUFEMUM5NjgzNDkiLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiJYNjJPR1NNSjN1dTNDeldUcGdWNXJSeVdnMGsifQ.eyJuYmYiOjE2OTM0MDY5OTksImV4cCI6MTY5MzQ5MzM5OSwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjoicHJlbWllciIsImNsaWVudF9pZCI6ImY2YzdiZWM4NDA2YjRjODVhYjUwNDNkNjhkYmIxMjM3Iiwic2NvcGUiOlsicHJlbWllciJdfQ.PfD6L5qlAuVpZCpuDTRossUZcQ4EL2fQNTqqw_Nxvf-Lo0v3ARn0jaF3PWP4Wq85SMkjwaihdsqAJE5AApQdoZXvU1XQym89XMxJ_aNcoirUZrGB96xvdKV6KKQ7-LN7ig-aaVdnzaYmn6rE0AK74oO7Pm87CLmtH7mvmAlGUMq1Z6zukIW-g2idkz-xEIPZQoaJwZfPd6qOKBJ7O4u9TUte4D2s2CWOXwIvqmbjzwFRXf5MKK2sg4fU8lhIfRcSUuwMyf17MHDrw_U0FODVfh0ZtSlQgEtrq1fMjuH4AlZbRwTvcNPhlI4Pgw_rBnM-lNFdTD-88FHzEbI3IDqkRg`
-  }
+  token = await getFsAccessToken();
   if (!token) return null;
 
   const url = "https://platform.fatsecret.com/rest/server.api"; 
