e32f85c522ac9029e1383a80380416f09f1c1aee
Chris Coudron <coudron@gmail.com>
2023-09-11 14:52:51 -0400

    Build fix

--- src/app/api/chat/getMessages/route.ts
+++ src/app/api/chat/getMessages/route.ts
@@ -1,49 +1,46 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
-import GetMessagesForUser from '../../../../database/GetMessagesForUser';
-import { Message } from "@prisma/client";
-import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server"
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth"
+import { GetMessagesForUser } from "../../../../database/GetMessagesForUser"
+import { Message } from "@prisma/client"
+import { getServerSession } from "next-auth"
 
 export async function GET(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-  
+  const session = await getServerSession(authOptions)
+
   if (!session) {
-    return new Response(
-      JSON.stringify({ error: 'Not authenticated' }),
-      {
-        status: 401,
-        headers: {
-          'Content-Type': 'application/json',
-        },
+    return new Response(JSON.stringify({ error: "Not authenticated" }), {
+      status: 401,
+      headers: {
+        "Content-Type": "application/json"
       }
-    )
+    })
   }
 
-  const userId = session.user.userId;
+  const userId = session.user.userId
 
   try {
-    let messages: Message[] = [];
-    console.log('userId', userId);
+    let messages: Message[] = []
+    console.log("userId", userId)
     if (userId) {
-      messages = await GetMessagesForUser(userId);
+      messages = await GetMessagesForUser(userId)
     }
     return new Response(JSON.stringify(messages), {
       status: 200,
       headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+        "Content-Type": "application/json"
+      }
+    })
   } catch (error) {
-    console.error(error);
-    const err = error as Error;
+    console.error(error)
+    const err = error as Error
     return new Response(
-      JSON.stringify({ error: err.message || 'Unable to fetch messages' }),
+      JSON.stringify({ error: err.message || "Unable to fetch messages" }),
       {
         status: 500,
         headers: {
-          'Content-Type': 'application/json',
-        },
+          "Content-Type": "application/json"
+        }
       }
-    );
+    )
   }
 }
