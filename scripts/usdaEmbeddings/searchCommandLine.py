bc243aa5011ff0cb63691f4e2841f4210463e307
Seb <sebgrubb@gmail.com>
2023-09-26 15:31:42 -0400

    gteEmbedding test

--- scripts/usdaEmbeddings/searchCommandLine.py
+++ scripts/usdaEmbeddings/searchCommandLine.py
@@ -2,9 +2,19 @@ import asyncio
 from FlagEmbedding import FlagModel
 from prisma import Prisma
 import time
+from gte_embedding import getGteEmbedding
+import torch
 
 model = FlagModel('BAAI/bge-base-en-v1.5', use_fp16=False)
 
+def construct_sentence(item):
+    if item['foodBrand']:
+        return f"{item['foodName']} - {item['foodBrand']}"
+    elif item['brandOwner']:
+        return f"{item['foodName']} - {item['brandOwner']}"
+    else:
+        return item['foodName']
+    
 async def search_similar_foods(prisma, sentence):
     embedding = model.encode([sentence])[0]
     embedding_list = embedding.tolist()
@@ -27,7 +37,7 @@ async def search_similar_foods(prisma, sentence):
     SELECT "fdcId", "foodName", "foodBrand", "brandOwner", cosine_similarity
     FROM ranked_foods
     ORDER BY cosine_similarity DESC
-    LIMIT 5;
+    LIMIT 10;
     """,
     embedding_list
     )
@@ -58,7 +68,7 @@ async def search_similar_foods_inner(prisma, sentence):
         FROM ranked_foods
         WHERE rnk = 1
         ORDER BY cosine_similarity DESC
-        LIMIT 5;
+        LIMIT 10;
         """,
         embedding_list
     )
@@ -76,7 +86,7 @@ async def search_similar_foods_not_unique(prisma, sentence):
         WHERE "bgeEmbedding" IS NOT NULL
         AND "foodBrand" <> ''
         ORDER BY cosine_similarity DESC
-        LIMIT 5
+        LIMIT 10
         """,
         embedding_list
     )
@@ -96,12 +106,29 @@ async def main():
         start_time = time.time()
         similar_foods = await search_similar_foods(prisma, search_term)
         elapsed_time = time.time() - start_time
-        
-        print(f"\nTop 5 similar foods(search took {elapsed_time:.3f} s):")
-        for i, food in enumerate(similar_foods):
-            print(f"{i+1}. {food['fdcId']} {food['foodName']} (Brand: {food.get('foodBrand', 'N/A')}, Owner: {food.get('brandOwner', 'N/A')}, Similarity: {food['cosine_similarity']})")
-    
+
+        # Generate GTE Embeddings for the search term
+        search_term_embedding_gte = getGteEmbedding([search_term])[0]
+
+        # Batch all food sentences
+        food_sentences = [construct_sentence(food) for food in similar_foods]
+
+        # Generate GTE Embeddings for all food items in a single batch
+        food_embeddings_gte = getGteEmbedding(food_sentences)
+
+        # Calculate GTE similarity
+        gte_similarities = torch.nn.functional.cosine_similarity(
+            search_term_embedding_gte.unsqueeze(0),
+            food_embeddings_gte
+        ).tolist()
+
+        print(f"\nTop 10 similar foods (search took {elapsed_time:.3f} s):")
+
+        for i, (food, gte_similarity) in enumerate(zip(similar_foods, gte_similarities)):
+            print(f"{i+1}. {food['fdcId']} {food['foodName']} (Brand: {food.get('foodBrand', 'N/A')}, Owner: {food.get('brandOwner', 'N/A')}, BGE Similarity: {food['cosine_similarity']:.3f}, GTE Similarity: {gte_similarity:.3f})")
+
     await prisma.disconnect()
+
 
 if __name__ == '__main__':
     asyncio.run(main())
