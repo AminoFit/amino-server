5e15b164a83f1c96fd8cfe40ab54c121ecd25ea6
Chris Coudron <coudron@gmail.com>
2023-11-02 00:24:47 -0400

    Update FoodTable.tsx

--- src/app/dashboard/FoodTable.tsx
+++ src/app/dashboard/FoodTable.tsx
@@ -32,7 +32,7 @@ export function FoodTable() {
     queryKey: ["foodData", formattedDate],
     queryFn: () => axios.get("/api/user/get-foods-on-date/" + formattedDate).then((res) => res.data),
     refetchIntervalInBackground: true,
-    refetchInterval: 1000 * 60 // 1 minute
+    refetchInterval: 1000 * 5 // every 5 seconds
   })
 
   const groups = _.chain(foods || [])
