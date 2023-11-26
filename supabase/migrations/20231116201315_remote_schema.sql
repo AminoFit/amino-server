502496bf702ec12edebe19fd1496e7f2d1f62c75
Seb <sebgrubb@gmail.com>
2023-11-26 17:38:48 -0600

    new user

---
+++ supabase/migrations/20231116201315_remote_schema.sql
@@ -0,0 +1,21 @@
+create extension if not exists "vector" with schema "extensions";
+
+
+--drop extension if exists "vector";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+  insert into public."User" (id, "fullName", "avatarUrl", "email")
+  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url', new.raw_user_meta_data->>'email');
+  return new;
+end;
+$function$
+;
+
+
