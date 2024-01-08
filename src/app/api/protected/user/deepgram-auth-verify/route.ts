0f93ec50c3be5fc4ebb1ff0cdc4b90f0d25dc104
Seb <sebgrubb@gmail.com>
2024-01-08 18:02:43 -0500

    Update Deepgram authentication in user route

--- src/app/api/protected/user/deepgram-auth-verify/route.ts
+++ src/app/api/protected/user/deepgram-auth-verify/route.ts
@@ -2,9 +2,13 @@ export const dynamic = "force-dynamic"
 
 import { GetAminoUserOnRequest } from "@/utils/supabase/GetUserFromRequest"
 import { NextRequest, NextResponse } from "next/server"
-import { createClient, CreateProjectKeySchema } from "@deepgram/sdk"
+import { createClient, CreateProjectKeySchema, CreateProjectKeyResponse } from "@deepgram/sdk"
 import { Tables } from "types/supabase"
 const DeepgramClient = createClient(process.env.DEEPGRAM_API_KEY || "DEEPGRAM_API_KEY")
+
+interface ExtendedCreateProjectKeyResponse extends CreateProjectKeyResponse {
+    expiration_date?: string;
+}
 
 export async function POST(request: NextRequest) {
   console.log("DEEPGRAM AUTH request")
@@ -22,14 +26,49 @@ export async function POST(request: NextRequest) {
   const options: CreateProjectKeySchema = {
     comment: "Key for amino user " + aminoUser.id,
     scopes: ["usage:write"],
-    time_to_live_in_seconds: 3600,
-    tags: ["temp-key"],
+    time_to_live_in_seconds: 600,
+    tags: ["temp-key"]
   }
-  const { result, error: keyError } = await DeepgramClient.manage.createProjectKey(process.env.DEEPGRAM_PROJECT_ID || "", options)
+  const { result, error: keyError } = await DeepgramClient.manage.createProjectKey(
+    process.env.DEEPGRAM_PROJECT_ID || "",
+    options
+  )
 
-    if (keyError) {
-        return new Response(keyError.message, { status: 400 })
-    }  
-  
+  if (keyError) {
+    return new Response(keyError.message, { status: 400 })
+  }
+
+  // if result has a expiration_date field, also return that
+  if ((result as ExtendedCreateProjectKeyResponse).expiration_date) {
+    return NextResponse.json({
+      api_key_id: result.api_key_id,
+      key: result.key,
+      expiration_date: (result as ExtendedCreateProjectKeyResponse).expiration_date
+    })
+  }
+
   return NextResponse.json({ api_key_id: result.api_key_id, key: result.key })
 }
+
+async function test() {
+  const options: CreateProjectKeySchema = {
+    comment: "Key for amino user " + "TEMP",
+    scopes: ["usage:write"],
+    time_to_live_in_seconds: 600,
+    tags: ["temp-key"]
+  }
+  const { result, error: keyError } = await DeepgramClient.manage.createProjectKey(
+    process.env.DEEPGRAM_PROJECT_ID || "",
+    options
+  )
+
+  if (keyError) {
+    return new Response(keyError.message, { status: 400 })
+  }
+  if ((result as ExtendedCreateProjectKeyResponse).expiration_date) {
+    console.log("result!", result)
+    return
+  }
+  console.log("result", result)
+}
+
