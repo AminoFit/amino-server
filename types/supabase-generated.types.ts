d37e86afc98b2932f9ac3365572980cf3641cc3e
Seb <sebgrubb@gmail.com>
2024-01-18 12:20:34 -0500

    new types

--- types/supabase-generated.types.ts
+++ types/supabase-generated.types.ts
@@ -7,31 +7,6 @@ export type Json =
   | Json[]
 
 export interface Database {
-  graphql_public: {
-    Tables: {
-      [_ in never]: never
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      graphql: {
-        Args: {
-          operationName?: string
-          query?: string
-          variables?: Json
-          extensions?: Json
-        }
-        Returns: Json
-      }
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
   public: {
     Tables: {
       _prisma_migrations: {
@@ -114,6 +89,7 @@ export interface Database {
           {
             foreignKeyName: "Account_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "User"
             referencedColumns: ["id"]
           }
@@ -310,12 +286,14 @@ export interface Database {
           {
             foreignKeyName: "FoodItem_messageId_fkey"
             columns: ["messageId"]
+            isOneToOne: false
             referencedRelation: "Message"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "FoodItem_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "User"
             referencedColumns: ["id"]
           }
@@ -344,12 +322,14 @@ export interface Database {
           {
             foreignKeyName: "FoodItemImages_foodImageId_fkey"
             columns: ["foodImageId"]
+            isOneToOne: false
             referencedRelation: "FoodImage"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "FoodItemImages_foodItemId_fkey"
             columns: ["foodItemId"]
+            isOneToOne: false
             referencedRelation: "FoodItem"
             referencedColumns: ["id"]
           }
@@ -384,6 +364,7 @@ export interface Database {
           {
             foreignKeyName: "IconQueue_requested_food_item_id_fkey"
             columns: ["requested_food_item_id"]
+            isOneToOne: false
             referencedRelation: "FoodItem"
             referencedColumns: ["id"]
           }
@@ -448,30 +429,35 @@ export interface Database {
           {
             foreignKeyName: "LoggedFoodItem_embeddingId_fkey"
             columns: ["embeddingId"]
+            isOneToOne: false
             referencedRelation: "foodEmbeddingCache"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "LoggedFoodItem_foodItemId_fkey"
             columns: ["foodItemId"]
+            isOneToOne: false
             referencedRelation: "FoodItem"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "LoggedFoodItem_messageId_fkey"
             columns: ["messageId"]
+            isOneToOne: false
             referencedRelation: "Message"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "LoggedFoodItem_servingId_fkey"
             columns: ["servingId"]
+            isOneToOne: false
             referencedRelation: "Serving"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "LoggedFoodItem_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "User"
             referencedColumns: ["id"]
           }
@@ -482,6 +468,7 @@ export interface Database {
           content: string
           createdAt: string
           function_name: string | null
+          hasimages: boolean
           id: number
           itemsProcessed: number | null
           itemsToProcess: number | null
@@ -496,6 +483,7 @@ export interface Database {
           content: string
           createdAt?: string
           function_name?: string | null
+          hasimages?: boolean
           id?: number
           itemsProcessed?: number | null
           itemsToProcess?: number | null
@@ -510,6 +498,7 @@ export interface Database {
           content?: string
           createdAt?: string
           function_name?: string | null
+          hasimages?: boolean
           id?: number
           itemsProcessed?: number | null
           itemsToProcess?: number | null
@@ -524,6 +513,7 @@ export interface Database {
           {
             foreignKeyName: "Message_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "User"
             referencedColumns: ["id"]
           }
@@ -555,6 +545,7 @@ export interface Database {
           {
             foreignKeyName: "Nutrient_foodItemId_fkey"
             columns: ["foodItemId"]
+            isOneToOne: false
             referencedRelation: "FoodItem"
             referencedColumns: ["id"]
           }
@@ -592,6 +583,7 @@ export interface Database {
           {
             foreignKeyName: "OpenAiUsage_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "User"
             referencedColumns: ["id"]
           }
@@ -629,6 +621,7 @@ export interface Database {
           {
             foreignKeyName: "Serving_foodItemId_fkey"
             columns: ["foodItemId"]
+            isOneToOne: false
             referencedRelation: "FoodItem"
             referencedColumns: ["id"]
           }
@@ -657,6 +650,7 @@ export interface Database {
           {
             foreignKeyName: "Session_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "User"
             referencedColumns: ["id"]
           }
@@ -688,6 +682,7 @@ export interface Database {
           {
             foreignKeyName: "SmsAuthCode_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "User"
             referencedColumns: ["id"]
           }
@@ -719,6 +714,7 @@ export interface Database {
           {
             foreignKeyName: "SmsMessage_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "User"
             referencedColumns: ["id"]
           }
@@ -831,6 +827,7 @@ export interface Database {
           {
             foreignKeyName: "User_id_fkey"
             columns: ["id"]
+            isOneToOne: true
             referencedRelation: "users"
             referencedColumns: ["id"]
           }
@@ -868,18 +865,60 @@ export interface Database {
           {
             foreignKeyName: "UserFavoriteFoodItem_foodItemId_fkey"
             columns: ["foodItemId"]
+            isOneToOne: false
             referencedRelation: "FoodItem"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "UserFavoriteFoodItem_servingId_fkey"
             columns: ["servingId"]
+            isOneToOne: false
             referencedRelation: "Serving"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "UserFavoriteFoodItem_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
+            referencedRelation: "User"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      UserMessageImages: {
+        Row: {
+          id: number
+          imagePath: string
+          messageId: number | null
+          uploadedAt: string | null
+          userId: string
+        }
+        Insert: {
+          id?: number
+          imagePath: string
+          messageId?: number | null
+          uploadedAt?: string | null
+          userId: string
+        }
+        Update: {
+          id?: number
+          imagePath?: string
+          messageId?: number | null
+          uploadedAt?: string | null
+          userId?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "UserMessageImages_messageId_fkey"
+            columns: ["messageId"]
+            isOneToOne: false
+            referencedRelation: "Message"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "usermessageimages_userid_fkey"
+            columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "User"
             referencedColumns: ["id"]
           }
@@ -920,18 +959,21 @@ export interface Database {
           {
             foreignKeyName: "userSubmittedBug_food_item_id_fkey"
             columns: ["food_item_id"]
+            isOneToOne: false
             referencedRelation: "FoodItem"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "userSubmittedBug_logged_food_id_fkey"
             columns: ["logged_food_id"]
+            isOneToOne: false
             referencedRelation: "LoggedFoodItem"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "userSubmittedBug_message_id_fkey"
             columns: ["message_id"]
+            isOneToOne: false
             referencedRelation: "Message"
             referencedColumns: ["id"]
           }
@@ -1019,54 +1061,6 @@ export interface Database {
           cosineSimilarity: number
         }[]
       }
-      hnswhandler: {
-        Args: {
-          "": unknown
-        }
-        Returns: unknown
-      }
-      ivfflathandler: {
-        Args: {
-          "": unknown
-        }
-        Returns: unknown
-      }
-      vector_avg: {
-        Args: {
-          "": number[]
-        }
-        Returns: string
-      }
-      vector_dims: {
-        Args: {
-          "": string
-        }
-        Returns: number
-      }
-      vector_norm: {
-        Args: {
-          "": string
-        }
-        Returns: number
-      }
-      vector_out: {
-        Args: {
-          "": string
-        }
-        Returns: unknown
-      }
-      vector_send: {
-        Args: {
-          "": string
-        }
-        Returns: string
-      }
-      vector_typmod_in: {
-        Args: {
-          "": unknown[]
-        }
-        Returns: number
-      }
     }
     Enums: {
       ActivityLevel:
@@ -1104,183 +1098,84 @@ export interface Database {
       [_ in never]: never
     }
   }
-  storage: {
-    Tables: {
-      buckets: {
-        Row: {
-          allowed_mime_types: string[] | null
-          avif_autodetection: boolean | null
-          created_at: string | null
-          file_size_limit: number | null
-          id: string
-          name: string
-          owner: string | null
-          owner_id: string | null
-          public: boolean | null
-          updated_at: string | null
-        }
-        Insert: {
-          allowed_mime_types?: string[] | null
-          avif_autodetection?: boolean | null
-          created_at?: string | null
-          file_size_limit?: number | null
-          id: string
-          name: string
-          owner?: string | null
-          owner_id?: string | null
-          public?: boolean | null
-          updated_at?: string | null
-        }
-        Update: {
-          allowed_mime_types?: string[] | null
-          avif_autodetection?: boolean | null
-          created_at?: string | null
-          file_size_limit?: number | null
-          id?: string
-          name?: string
-          owner?: string | null
-          owner_id?: string | null
-          public?: boolean | null
-          updated_at?: string | null
-        }
-        Relationships: []
-      }
-      migrations: {
-        Row: {
-          executed_at: string | null
-          hash: string
-          id: number
-          name: string
-        }
-        Insert: {
-          executed_at?: string | null
-          hash: string
-          id: number
-          name: string
-        }
-        Update: {
-          executed_at?: string | null
-          hash?: string
-          id?: number
-          name?: string
-        }
-        Relationships: []
-      }
-      objects: {
-        Row: {
-          bucket_id: string | null
-          created_at: string | null
-          id: string
-          last_accessed_at: string | null
-          metadata: Json | null
-          name: string | null
-          owner: string | null
-          owner_id: string | null
-          path_tokens: string[] | null
-          updated_at: string | null
-          version: string | null
-        }
-        Insert: {
-          bucket_id?: string | null
-          created_at?: string | null
-          id?: string
-          last_accessed_at?: string | null
-          metadata?: Json | null
-          name?: string | null
-          owner?: string | null
-          owner_id?: string | null
-          path_tokens?: string[] | null
-          updated_at?: string | null
-          version?: string | null
-        }
-        Update: {
-          bucket_id?: string | null
-          created_at?: string | null
-          id?: string
-          last_accessed_at?: string | null
-          metadata?: Json | null
-          name?: string | null
-          owner?: string | null
-          owner_id?: string | null
-          path_tokens?: string[] | null
-          updated_at?: string | null
-          version?: string | null
-        }
-        Relationships: [
-          {
-            foreignKeyName: "objects_bucketId_fkey"
-            columns: ["bucket_id"]
-            referencedRelation: "buckets"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      can_insert_object: {
-        Args: {
-          bucketid: string
-          name: string
-          owner: string
-          metadata: Json
-        }
-        Returns: undefined
-      }
-      extension: {
-        Args: {
-          name: string
-        }
-        Returns: string
-      }
-      filename: {
-        Args: {
-          name: string
-        }
-        Returns: string
-      }
-      foldername: {
-        Args: {
-          name: string
-        }
-        Returns: unknown
-      }
-      get_size_by_bucket: {
-        Args: Record<PropertyKey, never>
-        Returns: {
-          size: number
-          bucket_id: string
-        }[]
-      }
-      search: {
-        Args: {
-          prefix: string
-          bucketname: string
-          limits?: number
-          levels?: number
-          offsets?: number
-          search?: string
-          sortcolumn?: string
-          sortorder?: string
-        }
-        Returns: {
-          name: string
-          id: string
-          updated_at: string
-          created_at: string
-          last_accessed_at: string
-          metadata: Json
-        }[]
-      }
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
 }
 
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
