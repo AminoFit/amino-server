ea86f296f3258f91260172502f2dc5d421c0390c
Seb <sebgrubb@gmail.com>
2024-02-13 15:59:01 -0500

    better logs

--- src/app/api/protected/user/process-message-quick-log/route.ts
+++ src/app/api/protected/user/process-message-quick-log/route.ts
@@ -10,30 +10,34 @@ export async function POST(
   console.log("QUICK LOG MESSAGE PROCESS POST request")
 
   const { aminoUser, error } = await GetAminoUserOnRequest()
-
+  console.log("request", JSON.stringify(request))
   if (error) {
-    console.log("request", JSON.stringify(request))
     console.error("Error getting amino user on request: ", error)
     return new Response(error, { status: 400 })
   }
 
-  const requestBody = await request.json();
+  const requestBody = await request.json()
   console.log(requestBody)
-  const { messageId } = requestBody;
+  const { messageId } = requestBody
   // Default consumedOn to the current date/time if not provided
-  const consumedOn = requestBody.consumedOn || new Date().toISOString();
-  const isMessageBeingEdited = requestBody.isMessageBeingEdited || false;
+  const consumedOn = requestBody.consumedOn || new Date().toISOString()
+  const isMessageBeingEdited = requestBody.isMessageBeingEdited || false
 
   if (!messageId || typeof messageId !== "number") {
     return new Response("Invalid message ID provided", { status: 400 })
-}
+  }
 
   if (!aminoUser) {
     return new Response("No amino user found?", { status: 400 })
   }
 
   // const messageResult = await QuickLogMessage(aminoUser, message)
-  let responseMessage = await GenerateResponseForQuickLog(aminoUser, messageId as number, consumedOn, isMessageBeingEdited)
+  let responseMessage = await GenerateResponseForQuickLog(
+    aminoUser,
+    messageId as number,
+    consumedOn,
+    isMessageBeingEdited
+  )
 
   return NextResponse.json(responseMessage)
 }
