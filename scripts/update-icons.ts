0293a41c379d3265790fb0cb40d794e54e4feb1c
Chris Coudron <coudron@gmail.com>
2024-02-13 11:27:18 -0500

    Cleanup icon script

--- scripts/update-icons.ts
+++ scripts/update-icons.ts
@@ -4,9 +4,9 @@ import { createAdminSupabase } from "@/utils/supabase/serverAdmin"
 console.log("This script will generate any missing food icons.")
 
 async function run() {
-  await cleanupNullFoodId()
+  // await cleanupNullFoodId()
   await addMissingFoodItems()
-  await removeDuplicateIconQueueEntries()
+  // await removeDuplicateIconQueueEntries()
 
   console.log("Done. Make sure amino images is running.")
 }
@@ -27,50 +27,50 @@ async function addMissingFoodItems() {
 
   await Promise.all(data.map((foodItem) => SendRequestToGenerateIcon(foodItem.id)))
 }
-async function cleanupNullFoodId() {
-  console.log("Cleaning up null food id")
-  const supabaseAdmin = createAdminSupabase()
-  const { error } = await supabaseAdmin.from("IconQueue").delete().is("requested_food_item_id", null)
-  if (error) {
-    console.log("Error fetching IconQueue entries:", error)
-    return
-  }
-  console.log(`Deleted null entries`)
-}
+// async function cleanupNullFoodId() {
+//   console.log("Cleaning up null food id")
+//   const supabaseAdmin = createAdminSupabase()
+//   const { error } = await supabaseAdmin.from("IconQueue").delete().is("requested_food_item_id", null)
+//   if (error) {
+//     console.log("Error fetching IconQueue entries:", error)
+//     return
+//   }
+//   console.log(`Deleted null entries`)
+// }
 
-const removeDuplicateIconQueueEntries = async () => {
-  // Fetch all IconQueue entries
-  const supabaseAdmin = createAdminSupabase()
-  let { data: iconQueueEntries, error } = await supabaseAdmin
-    .from("IconQueue")
-    .select("*")
-    .order("result", { ascending: true })
+// const removeDuplicateIconQueueEntries = async () => {
+//   // Fetch all IconQueue entries
+//   const supabaseAdmin = createAdminSupabase()
+//   let { data: iconQueueEntries, error } = await supabaseAdmin
+//     .from("IconQueue")
+//     .select("*")
+//     .order("result", { ascending: true })
 
-  if (error) {
-    console.error("Error fetching IconQueue entries:", error)
-    return
-  }
+//   if (error) {
+//     console.error("Error fetching IconQueue entries:", error)
+//     return
+//   }
 
-  const entriesToDelete = []
-  const foundFoodItemIds = new Set()
+//   const entriesToDelete = []
+//   const foundFoodItemIds = new Set()
 
-  // Identify duplicate entries
-  for (const entry of iconQueueEntries || []) {
-    if (entry.requested_food_item_id && foundFoodItemIds.has(entry.requested_food_item_id)) {
-      entriesToDelete.push(entry.id)
-    } else {
-      foundFoodItemIds.add(entry.requested_food_item_id)
-    }
-  }
+//   // Identify duplicate entries
+//   for (const entry of iconQueueEntries || []) {
+//     if (entry.requested_food_item_id && foundFoodItemIds.has(entry.requested_food_item_id)) {
+//       entriesToDelete.push(entry.id)
+//     } else {
+//       foundFoodItemIds.add(entry.requested_food_item_id)
+//     }
+//   }
 
-  // Delete duplicate entries
-  for (const entryId of entriesToDelete) {
-    const { error } = await supabaseAdmin.from("IconQueue").delete().match({ id: entryId })
+//   // Delete duplicate entries
+//   for (const entryId of entriesToDelete) {
+//     const { error } = await supabaseAdmin.from("IconQueue").delete().match({ id: entryId })
 
-    if (error) {
-      console.error(`Error deleting entry with id ${entryId}:`, error)
-    } else {
-      console.log(`Deleted duplicate entry with id ${entryId}`)
-    }
-  }
-}
+//     if (error) {
+//       console.error(`Error deleting entry with id ${entryId}:`, error)
+//     } else {
+//       console.log(`Deleted duplicate entry with id ${entryId}`)
+//     }
+//   }
+// }
