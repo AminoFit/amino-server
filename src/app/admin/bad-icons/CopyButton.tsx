8cacc854a913830a295a0f673f15c33bda47cfdf
Chris Coudron <coudron@gmail.com>
2024-02-26 14:08:00 -0500

    Update admin icon page

--- src/app/admin/bad-icons/CopyButton.tsx
+++ src/app/admin/bad-icons/CopyButton.tsx
@@ -1,17 +1,18 @@
-'use client'
+"use client"
+
+import { useState } from "react"
+import CopyToClipboard from "react-copy-to-clipboard"
 
 export function CopyButton({ name }: { name: string }) {
+  const [status, setStatus] = useState("Copy Prompt")
   return (
-    <a
-      href="#"
-      className="text-indigo-600 hover:text-indigo-900"
-      onClick={() =>
-        navigator.clipboard.writeText(
-          `Generate a food icon of ${name}. It should be a small icon. On a white background. bold thick lines. It should be in color, but a light color pallet with no more than 3 colors. --no realistic. photo. face. gradients. detail. plate`
-        )
-      }
+    <CopyToClipboard
+      text={`Generate a food icon of ${name}. It should be a small icon. On a white background. bold thick lines. It should be in color, but a light color pallet with no more than 3 colors. --no realistic. photo. face. gradients. detail. plate`}
+      onCopy={() => setStatus("Copied!")}
     >
-      Copy Prompt
-    </a>
+      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+        {status}
+      </a>
+    </CopyToClipboard>
   )
 }
