c7a7b278fb27c371e43e7ca8053256e1c0610b69
Chris Coudron <coudron@gmail.com>
2024-02-26 12:23:35 -0500

    Update icons to use new script (#48)

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,7 +1,11 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from "@supabase/ssr"
+import { Database } from "types/supabase-generated.types"
 
-export const createClientSupabase = () =>
-  createBrowserClient(
+export const createClientSupabase = () => {
+  console.log("process.env.NEXT_PUBLIC_SUPABASE_URL", process.env.NEXT_PUBLIC_SUPABASE_URL)
+  console.log("process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
+}
---
+++ supabase/migrations/20240226164638_add_similarity_on_FoodItemImages.sql
@@ -0,0 +1,3 @@
+alter table "public"."FoodItemImages" add column "similarity" real not null default '0'::real;
+
+
