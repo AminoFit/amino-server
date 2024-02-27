6e968e081dc8ca34f1509b5b8d213dd02c168206
Chris Coudron <coudron@gmail.com>
2024-02-26 20:11:05 -0500

    Update CopyButton.tsx

--- src/app/admin/bad-icons/CopyButton.tsx
+++ src/app/admin/bad-icons/CopyButton.tsx
@@ -7,7 +7,7 @@ export function CopyButton({ name }: { name: string }) {
   const [status, setStatus] = useState("Copy Prompt")
   return (
     <CopyToClipboard
-      text={`Generate a food icon of ${name}. It should be a small icon. On a white background. bold thick lines. It should be in color, but a light color pallet with no more than 3 colors. --no realistic. photo. face. gradients. detail. plate`}
+      text={`${name}. Generate a food icon. It should be a small icon. On a white background. bold thick lines. It should be in color, but a light color pallet with no more than 3 colors. --no realistic. photo. face. gradients. detail. plate`}
       onCopy={() => {
         setStatus("Copied!")
         console.log("Copied")
