4d792f8742c9693a2a0d803c35f40f6c49fed98f
Seb <sebgrubb@gmail.com>
2025-09-18 15:22:22 -0400

    Update allowedUserIds.ts
    fix build

--- src/utils/admin/allowedUserIds.ts
+++ src/utils/admin/allowedUserIds.ts
@@ -5,7 +5,5 @@ export const ADMIN_ALLOWED_USER_IDS = [
   "2cf908ed-90a2-4ecd-a5f3-14b3a28fb05b",
   "6b005b82-88a5-457b-a1aa-60ecb1e90e21",
   "0e99c4d8-0c40-4399-8565-8379ebfffc49",
-  "3fea3294-9dcf-4acf-954b-6648028825ea"
-] as const
-
-export type AdminAllowedUserId = (typeof ADMIN_ALLOWED_USER_IDS)[number]
+  "3fea3294-9dcf-4acf-954b-6648028825ea",
+]
